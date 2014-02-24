ee71ed99a8c75ec72b82d6b851e94fbc3cf882c1
Darren Huang <psyren89@gmail.com>
2014-02-24 09:26:36 +0800

    Updated DLC for HAWX 2

--- dlc.js
+++ dlc.js
@@ -1631,8 +1631,9 @@ postMessage({dlc:{
 		"Splice: Epilogue Soundtrack", 
 		"Splice Soundtrack"
 	], 
-	"Tom Clancy's H.A.W.X.&reg; 2": [
-		"Tom Clancy's H.A.W.X.&reg; 2 - Open Skies Expansion Pack"
+	"Tom Clancy's H.A.W.X. 2": [
+		"Tom Clancy's H.A.W.X.&reg; 2 - Open Skies Expansion Pack",
+		"Tom Clancy's H.A.W.X. 2 - Open Skies Expansion Pack"
 	], 
 	"Tom Clancy's Splinter Cell Conviction Deluxe Edition": [
 		"Tom Clancy's Splinter Cell Conviction Insurgency Pack"
