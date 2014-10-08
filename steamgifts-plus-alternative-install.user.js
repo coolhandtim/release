736091dd883aa860f54359dce159cf0286760701
coolhandtim <coolhandtim@users.noreply.github.com>
2014-10-08 02:24:53 -0500

    Update steamgifts-plus-alternative-install.user.js

--- steamgifts-plus-alternative-install.user.js
+++ steamgifts-plus-alternative-install.user.js
@@ -14,6 +14,6 @@
 if (window.top == window) {
 	var steamgiftsPlus = document.createElement('script');
 	steamgiftsPlus.type = 'text/javascript';
-	steamgiftsPlus.src = 'http://github.com/coolhandtim/release/raw/master/steamgifts-plus.js';
+	steamgiftsPlus.src = 'http://github.com/coolhandtim/release/raw/master/steamgifts-plus.min.js';
 	document.body.appendChild(steamgiftsPlus);
 }
